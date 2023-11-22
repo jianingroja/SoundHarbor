0298b0673d2164b4961671cf7c17f95d70519731
Elena Gonzalez <egonzalezcm08@gmail.com>
2023-11-22 17:58:24 +0100

    feat: test fetchTopItems

--- __test__/lib/services/spotifyServices.test.js
+++ __test__/lib/services/spotifyServices.test.js
@@ -1,5 +1,8 @@
 import { beforeEach } from 'node:test';
-import { generateSpotifyHeader } from '../../../src/lib/services/spotifyServices.ts';
+import {
+  generateSpotifyHeader,
+  fetchTopItems,
+} from '../../../src/lib/services/spotifyServices.ts';
 import fetchMock from 'jest-fetch-mock';
 
 fetchMock.enableMocks();
@@ -16,5 +19,36 @@ describe('generateSpotifyHeader', () => {
     };
 
     expect(generateSpotifyHeader(accessToken)).toEqual(expectedHeader);
+  });
+});
+
+describe('fetchTopItems', () => {
+  it('should fetch top items correctly', async () => {
+    const mockItems = [{ id: 1, name: 'Test Item' }];
+    fetchMock.mockResponseOnce(JSON.stringify({ items: mockItems }));
+
+    const type = 'artists';
+    const accessToken = 'testAccessToken';
+    const limit = 10;
+
+    const items = await fetchTopItems(type, accessToken, limit);
+
+    expect(fetchMock).toHaveBeenCalledWith(
+      `https://api.spotify.com/v1/me/top/${type}?limit=${limit}`,
+      { headers: { Authorization: 'Bearer testAccessToken' } }
+    );
+    expect(items).toEqual(mockItems);
+  });
+
+  it('should handle fetch errors', async () => {
+    fetchMock.mockReject(new Error('Failed to fetch'));
+
+    const type = 'artists';
+    const accessToken = 'testAccessToken';
+    const limit = 10;
+
+    await expect(
+      fetchTopItems(type, accessToken, limit)
+    ).resolves.toBeUndefined();
   });
 });
